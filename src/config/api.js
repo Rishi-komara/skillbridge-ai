7117cddbcc22ea7c58a47268a48e55d16e861f62
Rishi-komara <rishivardhankomara@gmail.com>
2026-09-19 14:27:49 +0530

    Configure production API URL

--- src/config/api.js
+++ src/config/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_BASE_URL ||
+    "http://localhost:5000/api",
 });
 
-// Add Firebase token to every request
 apiClient.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("skillbridge-token");
@@ -15,9 +16,7 @@ apiClient.interceptors.request.use(
 
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default apiClient;
